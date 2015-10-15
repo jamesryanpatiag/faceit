9f4ca35ab347da466fcf97a319cb58207a1ea9c0
raiqee <rcsobere@yahoo.com>
2015-10-15 11:48:21 +0800

    Merge Log In and Registration Models

---
+++ src/com/group3/faceit/model/user/UserModel.java
@@ -0,0 +1,70 @@
+package com.group3.faceit.model.user;
+
+public class UserModel {
+
+	private String firstname;
+	private String middlename;
+	private String lastname;
+	private String username;
+	private String password;
+	private String birthdate;
+	private String gender;
+	private String address;
+	private String mobile;
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getMiddlename() {
+		return middlename;
+	}
+	public void setMiddlename(String middlename) {
+		this.middlename = middlename;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	
+}
