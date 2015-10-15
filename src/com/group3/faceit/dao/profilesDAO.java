2677eae45503dd227b50cfb96a05c998a302a637
josh.siscar <joshua.siscar@amdatex.com>
2015-10-15 11:58:51 +0800

    adding missing dao file

---
+++ src/com/group3/faceit/dao/profilesDAO.java
@@ -0,0 +1,44 @@
+package com.group3.faceit.dao;
+import java.sql.*;
+import java.util.*;
+import java.util.Date;
+import java.text.*;
+
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+
+import com.group3.faceit.model.newsfeed.PostModel;
+
+public class profilesDAO {
+	public static PreparedStatement pst;
+	public static CallableStatement cst;
+	public static ResultSet rs;
+	public static String query = "";
+	public static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+	private static final JDialog dialog = new JDialog();
+	
+	
+	public List<PostModel> getPosts(int sessionUserId, Connection conn){
+		List<PostModel> posts = new ArrayList<PostModel>();
+		
+		query = "{CALL newsfeedPosts(?)}"; //SHOULD HAVE PARAMETER SESSION.USERID
+		
+		try {
+			cst = conn.prepareCall(query);
+			cst.setInt(1, sessionUserId);
+			rs = cst.executeQuery();
+			while (rs.next()){
+				PostModel modelpost = new PostModel();
+				modelpost.setDatecreated(rs.getString("datecreated"));
+				modelpost.setDescription(rs.getString("description"));
+				modelpost.setFullname(rs.getString("fullname"));
+				modelpost.setPostid(rs.getInt("postid"));
+				posts.add(modelpost);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return posts;
+	}
+}
